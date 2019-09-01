1b9eb2c75215d00c0b0947059a6435c07fea4360
dianynf <dianbugas@gmail.com>
2019-09-01 21:26:48 +0700

    belajar laravel

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    <title>Beastudi</title>
+    <title>about</title>
 </head>
 
 <body>
@@ -31,7 +31,7 @@
     <div class="container">
         <div class="row">
             <div class="col-10">
-                <h1 class="mt-3">dian</h1>
+                <h1 class="mt-3">About</h1>
             </div>
         </div>
     </div>
