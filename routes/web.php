a84898659dac5ec0cb88efbe321ca2ed2153ae2b
dianynf <dianbugas@gmail.com>
2019-09-02 16:28:54 +0700

    contoller mahasiswa

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,15 @@
 |
 */
 
-//root
-Route::get('/', function () {
-    return view('index');
-});
+// //root
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::get('/about', function () {
-    $nama = 'Muhammad Ardiansyah';
-    return view('about', ['nama' => $nama]);
-});
+// Route::get('/about', function () {
+//     $nama = 'Muhammad Ardiansyah';
+//     return view('about', ['nama' => $nama]);
+
+Route::get('/', 'PagesController@home');
+Route::get('/about', 'PagesController@about');
+// });
