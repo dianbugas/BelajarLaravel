6a9564403c898d3199edca5be6a07642f8da75d6
dianynf <dianbugas@gmail.com>
2020-01-21 22:12:59 +0700

    LARAVEL 6 API

---
+++ app/Http/Controllers/Api/QuoteController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\QuoteResource;
+use App\Quote;
+use Illuminate\Http\Request;
+
+class QuoteController extends Controller
+{
+    public function store(Request $request){
+        $this->validate($request, [
+            'message' => 'required',
+        ]);
+
+        $quote = Quote::create([
+            'user_id' => auth()->id(),
+            'message' => $request->message, 
+        ]);
+
+        return new QuoteResource($quote);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@
 
 Route::post('register','Api\RegisterController@action');
 Route::post('login','Api\LoginController@action');
+Route::get('me','Api\UserController@me')->middleware('auth:api');
+Route::post('quote','Api\QuoteController@store')->middleware('auth:api');
